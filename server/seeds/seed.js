82a5e432b45bf8b723d0b3689d671ffa02328e8f
Mark Anthony <manthonyamaro@yahoo.com>
2023-09-25 21:24:56 -0500

    added code from auth.js from emily and set up the seed and module index while geting the server set up

---
+++ server/seeds/seed.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const db = require('../config/connection');
+
+const userSeeds = require('./userSeeds');
+const capsuleSeeds = require('./capsuleSeeds');
+
+db.once('open', async () => {
+  try {
+    await mongoose.connection.db.dropDatabase();
+
+    await userSeeds();
+    await capsuleSeeds();
+
+    console.log('All seeds successful!');
+    process.exit(0);
+  } catch (err) {
+    throw err;
+  }
+});
