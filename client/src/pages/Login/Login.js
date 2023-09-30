3cf89e2f0c8587c0f6e01088c65966091154e8ba
Emily Rose Arizcorreta <rosesandbooks89@gmail.com>
2023-09-30 13:20:16 -0500

    Revert "added changes to save data from register/login"

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,31 +1,20 @@
 import React, { useState } from 'react';
 import { Form, Input, Button, Modal, Typography, message } from 'antd';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../../utils/mutations';
-
 const { Title } = Typography;
 const Login = ({ visible, onClose }) => {
   const [loading, setLoading] = useState(false);
-  const [login] = useMutation(LOGIN_USER);
-
-  const onFinish = async (values) => {
+  const onFinish = (values) => {
     setLoading(true);
-    console.log('Form values:', values);
-    try {
-      const { data } = await login({ variables: { ...values } });
-
-      if (data.login) {
+    console.log('Form values:', values); // Add this line for debugging
+    setTimeout(() => {
+      setLoading(false);
+      if (values.username === 'user' && values.password === 'password') {
         message.success('Login successful');
         onClose();
       } else {
         message.error('Invalid username or password');
       }
-    } catch (error) {
-      console.error("Authentication Error:", error);
-      message.error('Invalid username or password');
-    } finally {
-      setLoading(false);
-    }
+    }, 1000);
   };
   return (
     <Modal
